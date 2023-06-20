4f3608629e4da4d2c265e5a610cf72f48f353e3b
PikaCode5900 <pableitor5900@gmail.com>
2023-06-20 10:49:44 +0200

    Comenzando a generar las vistas

--- routes/aulas.js
+++ routes/aulas.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const pool = require('../database');
 
-router.get('/', function(req, res, next) {
-  res.send('respond with aulas');
+router.get('/add', function(req, res, next) {
+  res.render('aulas/add');
 });
+
+router.post('/add', function(req, res, next) {
+    res.send('recibido');
+  });
 
 module.exports = router;
